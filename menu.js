85052ec2b0e4e98f3bc8f256ec0b1d449b808481
Lode <lvandeve@gmail.com>
2015-07-27 23:02:29 +0200

    version numbers

--- menu.js
+++ menu.js
@@ -48,7 +48,7 @@ function renderPreScreen(px, py, standardButtonFun, randomButtonFun, beginnerBut
       + 'Programmed by Lode Vandevenne.<br/>'
       + 'AI tweaks by Lou New.<br/>'
       + 'Drawings by Giordano Segatta.<br/>'
-      + 'version: v.20150509<br/>'
+      + 'version: v.20150709<br/>'
       + 'Links:<br/>'
       + 'TM on BGG: <a href="http://boardgamegeek.com/boardgame/120677/terra-mystica">http://boardgamegeek.com/boardgame/120677/terra-mystica</a><br/>'
       + 'Snellman: <a href="http://terra.snellman.net/">http://terra.snellman.net/</a><br/>'
